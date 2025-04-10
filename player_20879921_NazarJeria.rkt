512b5f08792e926c337ed0d3550bacc1ea3ab48f
Codigoolimpo <sahid2002@gmail.com>
2025-04-09 21:56:06 -0400

    Inicio lab

---
+++ player_20879921_NazarJeria.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(provide jugador)
+#|
+Representación TDA Jugador:; Se utiliza una lista donde:
+- Posición 1 (car): id (Integer)
+- Posición 2 (cadr): nombre (String)
+- Posición 3 (caddr): dinero (Integer)
+- Posición 4 (cadddr): propiedades (List) ; Lista vacía inicialmente
+- Posición 5: posicion (Integer) ; Posición en el tablero
+- Posición 6: estaEnCarcel (Boolean)
+- Posición 7: totalCartasSalirCarcel (Integer)
+|#
