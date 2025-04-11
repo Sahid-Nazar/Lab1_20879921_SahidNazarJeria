6a850817e4a91c3d9c8f6514589a657f2cd76a51
Sahid Nazar <sahid2002@gmail.com>
2025-04-11 15:23:35 -0400

    Actualizacion2 tda board

--- board_20879921_NazarJeria.rkt
+++ board_20879921_NazarJeria.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(provide tablero)
+(provide tablero tablero-agregar-propiedades)
 ; Representación TDA Tablero:
 ; Se utiliza una lista principal que contiene cuatro sub-listas:
 ; 1. (car lista)      => Lista de propiedades (List of property). La posición se manejará después.
@@ -11,6 +11,25 @@
 ; Dominio: propiedades(List) X cartas-suerte(List) X cartas-comunidad(List) X casillas-especiales(List)
 ; Recorrido: tablero (Estructura de lista representando el tablero según la definición de representación)
 ; Tipo recursión: No aplica
-
 (define (tablero propiedades cartas-suerte cartas-comunidad casillas-especiales)
   (list propiedades cartas-suerte cartas-comunidad casillas-especiales))
+
+
+
+; Descripción: Agrega una lista de propiedades (cada una asociada a una posición) al TDA tablero. Devuelve un nuevo tablero con las propiedades añadidas.
+; Dominio: tablero(tablero) X propiedades-con-posicion(List) ; donde cada elemento es un par (propiedad . posicion)
+; Recorrido: tablero (Una nueva estructura de lista representando el tablero actualizado)
+; Tipo recursión: No aplica
+(define (tablero-agregar-propiedades tablero-actual lista-propiedades-con-posicion)
+  ; Lo primero que haremos sera desarmar el tablero viejo extrayendo las listas que ya estaban en el tablero
+  (let ((propiedades-viejas (car tablero-actual))
+        (cartas-suerte      (cadr tablero-actual))
+        (cartas-comunidad   (caddr tablero-actual))
+        (casillas-especiales (cadddr tablero-actual)))
+    ; Ahora creamos una lista que contenga SÓLO las propiedades de la lista de entrada
+    ; (ignorando la posición por ahora, usando 'map' y 'car')
+    (let* ((propiedades-nuevas-solas   (map car lista-propiedades-con-posicion))
+           ; Unimos propiedades viejas con propiedades nuevas 
+           (propiedades-actualizadas (append propiedades-viejas propiedades-nuevas-solas)))
+      ; LLamamos a la funcion constructora para armar el tablero con las entradas actualizadas
+      (tablero propiedades-actualizadas cartas-suerte cartas-comunidad casillas-especiales))))
