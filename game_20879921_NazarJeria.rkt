ee63414d7917f15ff057e812b7069bd96a249062
Sahid Nazar <sahid2002@gmail.com>
2025-04-22 23:12:15 -0400

    Implemento de  TDA game constructores and selectores

--- game_20879921_NazarJeria.rkt
+++ game_20879921_NazarJeria.rkt
@@ -1,5 +1,8 @@
 #lang racket
-(provide juego juego-agregar-jugador juego-obtener-jugador-actual)
+(provide juego juego-agregar-jugador juego-obtener-jugador-actual
+         juego-lista-jugadores juego-tablero juego-dineroBanco juego-numeroDados
+         juego-indiceTurnoActual juego-tasaImpuesto juego-maximoCasas
+         juego-maximoHoteles juego-estadoJuego)
 ; Representación TDA Juego:
 ; Se utiliza una lista donde cada posición representa:
 ; 1. Lista de jugadores (List of jugador)
@@ -18,6 +21,72 @@
 ; Tipo recursión: No aplica
 (define (juego jugadores tablero dineroBanco numeroDados turnoActual tasaImpuesto maximoCasas maximoHoteles estadoJuego)
   (list jugadores tablero dineroBanco numeroDados turnoActual tasaImpuesto maximoCasas maximoHoteles estadoJuego))
+
+
+; Selectores TDA Juego
+
+; Descripción: Obtiene la lista de jugadores de la partida.
+; Dominio: juego(juego)
+; Recorrido: List ; Lista de TDA jugador
+; Tipo recursión: No aplica
+(define (juego-lista-jugadores un-juego)
+  (car un-juego)) ; 1er elemento
+
+; Descripción: Obtiene el TDA tablero de la partida.
+; Dominio: juego(juego)
+; Recorrido: tablero
+; Tipo recursión: No aplica
+(define (juego-tablero un-juego)
+  (cadr un-juego)) ; 2do elemento
+
+; Descripción: Obtiene la cantidad de dinero actual en el banco.
+; Dominio: juego(juego)
+; Recorrido: Integer
+; Tipo recursión: No aplica
+(define (juego-dineroBanco un-juego)
+  (caddr un-juego)) ; 3er elemento
+
+; Descripción: Obtiene el número de dados que se usan en la partida.
+; Dominio: juego(juego)
+; Recorrido: Integer
+; Tipo recursión: No aplica
+(define (juego-numeroDados un-juego)
+  (cadddr un-juego)) ; 4to elemento
+
+; Descripción: Obtiene el índice (0-based) del jugador del turno actual.
+; Dominio: juego(juego)
+; Recorrido: Integer
+; Tipo recursión: No aplica
+(define (juego-indiceTurnoActual un-juego) ; Cambié el nombre para claridad
+  (list-ref un-juego 4)) ; 5to elemento (índice 4)
+
+; Descripción: Obtiene la tasa de impuesto (probablemente porcentaje).
+; Dominio: juego(juego)
+; Recorrido: Integer
+; Tipo recursión: No aplica
+(define (juego-tasaImpuesto un-juego)
+  (list-ref un-juego 5)) ; 6to elemento (índice 5)
+
+; Descripción: Obtiene el número máximo de casas construibles por propiedad.
+; Dominio: juego(juego)
+; Recorrido: Integer
+; Tipo recursión: No aplica
+(define (juego-maximoCasas un-juego)
+  (list-ref un-juego 6)) ; 7mo elemento (índice 6)
+
+; Descripción: Obtiene el número máximo de hoteles construibles por propiedad.
+; Dominio: juego(juego)
+; Recorrido: Integer
+; Tipo recursión: No aplica
+(define (juego-maximoHoteles un-juego)
+  (list-ref un-juego 7)) ; 8vo elemento (índice 7)
+
+; Descripción: Obtiene el estado actual de la partida (ej: "preparacion", "en curso").
+; Dominio: juego(juego)
+; Recorrido: String
+; Tipo recursión: No aplica
+(define (juego-estadoJuego un-juego)
+  (list-ref un-juego 8)) ; 9no elemento (índice 8)
 
 
 
